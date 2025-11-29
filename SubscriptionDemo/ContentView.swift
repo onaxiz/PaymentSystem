c6637c4ea0021028e1b1f6a3e429842d31637385
emaksimova <maksimovaswift@gmail.com>
2025-11-30 02:28:01 +0300

    Initial Commit

---
+++ SubscriptionDemo/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  SubscriptionDemo
+//
+//  Created by Евгения Максимова on 30.11.2025.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ SubscriptionDemo/SubscriptionDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  SubscriptionDemoApp.swift
+//  SubscriptionDemo
+//
+//  Created by Евгения Максимова on 30.11.2025.
+//
+
+import SwiftUI
+
+@main
+struct SubscriptionDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
